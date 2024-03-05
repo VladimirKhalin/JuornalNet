af67df5e2c1e355cd6a1e72071455579dedab411
VK <art.nebar@mail.ru>
2024-03-05 12:58:45 +0300

    Initial Commit

---
+++ JuornalNet/JuornalNetApp.swift
@@ -0,0 +1,17 @@
+//
+//  JuornalNetApp.swift
+//  JuornalNet
+//
+//  Created by Vladimir Khalin on 05.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct JuornalNetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
