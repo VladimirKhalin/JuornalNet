91979e998df9aae8c46462b500a0e68ac135b1f7
VK <art.nebar@mail.ru>
2024-03-06 08:49:23 +0300

    Удалил мусор из TabBar. Форматирование.

--- JuornalNet/TapBar/TapBarView.swift
+++ JuornalNet/TapBar/TapBarView.swift
@@ -9,7 +9,6 @@ import SwiftUI
 import Combine
 
 struct TapBarView: View {
-   // @ObservedObject var router = TabRouter()
     @State private var two : String = "Two"
     @State private var selection: Int = 1
     
@@ -20,34 +19,25 @@ struct TapBarView: View {
     var body: some View {
        
         TabView(selection: $selection ) {
-            //TabView(selection: $router.screen ) {
             ArticlesView()
                 .tag(1)
                 .tabItem { Label("Articles", systemImage: "doc.fill") }
-             //   .environmentObject(router)
             
-           
             EventsView()
                 .tag(2)
                 .tabItem { Label("Events", systemImage: "trophy.fill") }
-               // .environmentObject(router)
-            
-            
+              
            ButgetView()
                 .tag(3)
                 .tabItem { Label("Butget", systemImage: "dollarsign.circle.fill") }
-              //  .environmentObject(router)
             
            PostsView()
                 .tag(4)
                 .tabItem { Label("Posts", systemImage: "doc.richtext.fill") }
-             //   .environmentObject(router)
             
             SettingsView()
                  .tag(5)
                  .tabItem { Label("Settings", systemImage: "gearshape.fill") }
-               //  .environmentObject(router)
-            
         }
         .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
         .preferredColorScheme(.dark)
